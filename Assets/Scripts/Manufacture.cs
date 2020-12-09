c564fa4e90c35207fb8d637490ba7d9bd3e5d217
a-samoylov <a.samoylovua@gmail.com>
2020-12-10 00:10:34 +0200

    Manufacture scale

--- Assets/Scripts/Manufacture.cs
+++ Assets/Scripts/Manufacture.cs
@@ -8,17 +8,17 @@ public class Manufacture : MonoBehaviour
     public GameObject productPrefab;
 
     private float _money = 0;
-    
+
     void Start()
     {
     }
 
     void Update()
     {
-        /*if (Input.GetKeyDown(KeyCode.Z))
+        if (Input.GetKeyDown(KeyCode.Z))
         {
-            CreateProduct();
-        }*/
+            SetBigSize();
+        }
 
         /*if (Exchange.IsPossibleSell())
         {
@@ -34,5 +34,31 @@ public class Manufacture : MonoBehaviour
         productPrefabPosition.y = 1;
 
         return Instantiate(productPrefab, productPrefabPosition, Quaternion.identity);
+    }
+
+    public void SetSmallSize()
+    {
+        Scale(new Vector3(1f, 1f, 1f));
+    }
+    
+    public void SetMidldeSize()
+    {
+        Scale(new Vector3(1.5f, 3f, 1.5f));
+    }
+    
+    public void SetBigSize()
+    {
+        Scale(new Vector3(2f, 5f, 2f));
+    }
+
+    private void Scale(Vector3 scale)
+    {
+        gameObject.transform.localScale = scale;
+
+        gameObject.transform.position = new Vector3(
+            gameObject.transform.position.x,
+            (scale.y / 2) * -1,
+            gameObject.transform.position.z
+        );
     }
 }
